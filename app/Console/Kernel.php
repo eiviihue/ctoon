cd19c7229bcd76c00d0ef2db85a4959f2d8c841a
Rafi Aqila Maulana <rafiaqila2005@gmail.com>
2025-10-24 10:27:37 +0700

    try deploy

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,12 +7,6 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     protected $commands = [
-        \App\Console\Commands\SyncAzureStorage::class,
-        \App\Console\Commands\MigrateToAzure::class,
-        \App\Console\Commands\FixAzurePaths::class,
-        \App\Console\Commands\ImportLocalComics::class,
-        \App\Console\Commands\ImportCovers::class,
-        \App\Console\Commands\AzCopyMigrate::class,
         \App\Console\Commands\SyncFromAzureStorage::class,
     ];
 
