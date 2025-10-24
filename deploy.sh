7e1ba09d07022545c4e83c892185d0f3fa37f821
Rafi Aqila Maulana <rafiaqila2005@gmail.com>
2025-10-24 13:08:52 +0700

    update

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Composer install
+composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+
+# Clear cache
+php artisan cache:clear
+php artisan config:clear
+
+# Storage link
+php artisan storage:link
+
+# Database migrations
+php artisan migrate --force
+
+# NPM Install & Build (if you have front-end assets)
+if [ -f "package.json" ]; then
+    npm install
+    npm run build
+fi
+
+php artisan az:sync-from-storage
+
+# Optimize
+php artisan optimize
