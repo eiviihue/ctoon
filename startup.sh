999b48283c2b3c7a11688de0d186bf7a4b0aa01a
Rafi Aqila Maulana <rafiaqila2005@gmail.com>
2025-10-27 01:06:18 +0700

    Update startup.sh to use Azure App Service conventions

--- startup.sh
+++ startup.sh
@@ -6,12 +6,21 @@ if [ -f "/home/php.ini" ]; then
     cp /home/php.ini /usr/local/etc/php/conf.d/php.ini
 fi
 
+# Enable Laravel maintenance mode
+php /home/site/wwwroot/artisan down --refresh=15 --secret="1630542a-246b-4b66-afa1-dd72a4c43515"
+
+# Run migrations
+php /home/site/wwwroot/artisan migrate --force
+
 # Clear and cache everything
-php /home/site/wwwroot/artisan cache:clear
-php /home/site/wwwroot/artisan route:cache
-php /home/site/wwwroot/artisan config:cache
+php /home/site/wwwroot/artisan optimize
 php /home/site/wwwroot/artisan view:cache
 
-# Restart nginx
-service nginx restart
+# Instead of restarting nginx directly, touch the restart marker
+touch /tmp/app_initialized
+mkdir -p /home/LogFiles
+echo "$(date): Initialization complete" >> /home/LogFiles/startup.log
+
+# Disable maintenance mode
+php /home/site/wwwroot/artisan up
 
